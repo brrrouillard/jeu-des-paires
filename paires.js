9d1b626a2ae0f18cb900d0fb8cf85bf2674c3ef3
Matthieu Brouillard <mbrouilla@gmail.com>
2018-10-18 16:38:25 +0200

    Add pairs, limitations and score

--- paires.js
+++ paires.js
@@ -1,20 +1,77 @@
 class Card {
     constructor(domElement, imgSrc) {
         this._domElement = domElement;
-        this._visible = false;
-        this._imgSrc = "/img/" + imgSrc;
+        this._imgSrc = "img/" + imgSrc;
+        this._found = false;
         this.addImgSrc();
-        console.log(this._domElement);
+        this.hide();
     }
+    // PUBLIC
     reveal() {
         this._visible = true;
+        this._domElement.style.backgroundSize = "cover";
     }
     hide() {
         this._visible = false;
+        this._domElement.style.backgroundSize = "0% 0%";
     }
+    // PRIVATE
     addImgSrc() {
-        let path = `url('${this._imgSrc}")`;
+        let path = "url(" + this._imgSrc + " )";
         this._domElement.style.backgroundImage = path;
     }
+    // GETTERS
+    get visible() {
+        return this._visible;
+    }
+    get found() {
+        return this._found;
+    }
+    get imgSrc() {
+        return this._imgSrc;
+    }
+    // SETTERS
+    set found(value) {
+        this._found = value;
+    }
 }
-let card1 = new Card(document.getElementById("1"), "anehihan.jpg");
+class Game {
+    static clickOnCard(event) {
+        let cardId = event.target.id;
+        let selectedCard = arr[cardId - 1]; // Select the card the player just clicked on
+        console.log(selectedCard);
+        if (selectedCard.visible && !selectedCard.found) { // If the card is already shown
+            selectedCard.hide();
+            Game._activeCardsCount--;
+            Game._currentCards.pop();
+        }
+        else if (!selectedCard.visible && Game._activeCardsCount < 2) {
+            selectedCard.reveal();
+            Game._activeCardsCount++;
+            Game._currentCards.push(selectedCard);
+            if (Game._activeCardsCount > 1) {
+                if (Game.areEquals(Game._currentCards[0], Game._currentCards[1])) { // If both cards are equals
+                    Game._currentCards[0].found = true;
+                    Game._currentCards[1].found = true;
+                    Game._pairFound++;
+                }
+            }
+        }
+        Game.updateStatus();
+    }
+    static areEquals(first, second) {
+        return first.imgSrc == second.imgSrc;
+    }
+    static updateStatus() {
+        document.getElementById("pair-found").innerHTML = "Pair founds : " + Game._pairFound;
+    }
+}
+Game._pairFound = 0;
+Game._activeCardsCount = 0;
+Game._currentCards = [];
+let arr = []; // Arrays of 14 items containing each card
+for (let i = 1; i <= 14; i++) {
+    let id = i.toString();
+    arr.push(new Card(document.getElementById(id), "anehihan.jpg"));
+    arr[i - 1]._domElement.addEventListener("click", Game.clickOnCard); // Hide on click
+}
--- paires.ts
+++ paires.ts
@@ -1,27 +1,83 @@
 class Card {
     _domElement: any;
     _visible: boolean;
+    _found: boolean;
     _imgSrc: string;
+
     constructor(domElement: any, imgSrc: string){
         this._domElement = domElement;
-        this._visible = false;
-        this._imgSrc = "/img/" + imgSrc;
+        this._imgSrc = "img/" + imgSrc;
+        this._found = false;
         this.addImgSrc();
-        console.log(this._domElement);
+        this.hide();
     }
-
-    reveal(): void {
+    // PUBLIC
+    public reveal(): void {
         this._visible = true;
+        this._domElement.style.backgroundSize = "cover";
     }
-    hide(): void {
+    public hide(): void {
         this._visible = false;
+        this._domElement.style.backgroundSize = "0% 0%";
     }
-
-    
-    addImgSrc(): void {
-        let path = `url('${this._imgSrc}")`;
+    // PRIVATE
+    private addImgSrc(): void {
+        let path = "url(" +  this._imgSrc + " )";
         this._domElement.style.backgroundImage = path;
     }
+    // GETTERS
+    get visible(): boolean {
+        return this._visible;
+    }
+    get found(): boolean {
+        return this._found;
+    }
+    get imgSrc() {
+        return this._imgSrc;
+    }
+    // SETTERS
+    set found(value) {
+        this._found = value;
+    }
 }
+class Game {
+    static _pairFound = 0;
+    static _activeCardsCount = 0;
+    static _currentCards = [];
+    static clickOnCard(event): void {   // Click handler
+        let cardId = event.target.id;
+        let selectedCard = arr[cardId-1]; // Select the card the player just clicked on
+        console.log(selectedCard);
+        if (selectedCard.visible && !selectedCard.found){ // If the card is already shown
+            selectedCard.hide();
+            Game._activeCardsCount--;
+            Game._currentCards.pop();
+        }
+        else if (!selectedCard.visible && Game._activeCardsCount < 2){     
+            selectedCard.reveal();
+            Game._activeCardsCount++;
+            Game._currentCards.push(selectedCard);
+            if(Game._activeCardsCount > 1){
+                if( Game.areEquals(Game._currentCards[0], Game._currentCards[1])){ // If both cards are equals
+                    Game._currentCards[0].found = true;
+                    Game._currentCards[1].found = true;
+                    Game._pairFound++;
+                }
+            }
+        }
+        Game.updateStatus();
+    }
 
-let card1 = new Card(document.getElementById("1"), "anehihan.jpg");
+    static areEquals(first, second): boolean { // Tells if 2 cards are identical
+        return first.imgSrc == second.imgSrc; 
+    }
+    static updateStatus(): void {
+        document.getElementById("pair-found").innerHTML = "Pair founds : " + Game._pairFound;
+    }
+}
+let arr = []; // Arrays of 14 items containing each card
+for (let i = 1; i <= 14; i++){
+    let id:string = i.toString();
+    arr.push(new Card(document.getElementById(id),"anehihan.jpg"));
+    arr[i-1]._domElement.addEventListener("click", Game.clickOnCard); // Hide on click
+}
